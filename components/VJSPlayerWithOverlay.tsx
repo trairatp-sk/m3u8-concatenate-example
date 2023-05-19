739937987545fd09b37382881bbf24495f1392ae
Trairat Punyawatthanunakool <samoooop@gmail.com>
2023-05-19 12:04:48 +0700

    Fix build failed

--- components/VJSPlayerWithOverlay.tsx
+++ components/VJSPlayerWithOverlay.tsx
@@ -41,6 +41,8 @@ const VideoInfoHeader = ({
 
 const VideoControl = ({
   isActive,
+  activeChapter,
+  setSeekToSec,
 }: {
   isActive: boolean;
   activeChapter?: string;
@@ -123,16 +125,14 @@ const VJSPlayerWithOverlay = ({
   };
 
   const handlePlayerReady = (player: Player) => {
-    playerRef.current = player;
-
-    // You can handle player events here, for example:
-    player.on("waiting", () => {
-      videojs.log("player is waiting");
-    });
-
-    player.on("dispose", () => {
-      videojs.log("player will dispose");
-    });
+    // playerRef.current = player;
+    // // You can handle player events here, for example:
+    // player.on("waiting", () => {
+    //   videojs.log("player is waiting");
+    // });
+    // player.on("dispose", () => {
+    //   videojs.log("player will dispose");
+    // });
   };
 
   const [isControlActive, setIsControlActive] = useState(true);
