807daf5441f3752ab362b399a1994f7cf6f1a179
Trairat Punyawatthanunakool <samoooop@gmail.com>
2023-05-22 23:58:50 +0700

    Change video src for smooth

--- pages/smooth.tsx
+++ pages/smooth.tsx
@@ -3,7 +3,7 @@ import VideoPlayerWithOverlay from "components/VideoPlayerWithOverlay";
 export default function VideoWithOverlay() {
   return (
     <VideoPlayerWithOverlay
-      videoSrc="https://pub-a745e3b4cca7460d96493738dbfc72f8.r2.dev/output.m3u8"
+      videoSrc="https://storage.googleapis.com/lc-smooth-playlist-poc/avc6sec100k/output.m3u8"
       vttSrc="/overlay.vtt"
     ></VideoPlayerWithOverlay>
   );
